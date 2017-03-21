b04c62186c2be5e951919e8e31bebc1b1294ab60
olko0715 <36Dkr845>
2017-03-21 13:45:15 +0200

    Fixed initial database query. Added ER diagram

--- scripts/initDB.sql
+++ scripts/initDB.sql
@@ -1,4 +1,4 @@
-DROP TABLE if EXISTS USER_ORDERS;
+DROP TABLE if EXISTS TEAMS;
 DROP TABLE if EXISTS USER_SKILLS;
 DROP TABLE if EXISTS SKILLS;
 DROP TABLE if EXISTS PAYMENTS;
@@ -16,62 +16,71 @@ CREATE TABLE SKILLS(
 
 CREATE TABLE USERS(
   user_id SERIAL PRIMARY KEY,
-  user_nickname VARCHAR(255) NOT NULL,
+  nickname VARCHAR(255),
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
   mail VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   role VARCHAR(255) NOT NULL,
-  status VARCHAR(255) NOT NULL,
-  registration_date DATE NOT NULL,
-  rating INT NOT NULL
+  status VARCHAR(255) NOT NULL /* set default*/,
+  registration_date TIMESTAMP DEFAULT current_timestamp,
+  rating FLOAT DEFAULT 0.0 NOT NULL
 );
+
 CREATE TABLE ORDERS(
-  order_id SERIAL PRIMARY KEY ,
-  customer INT NOT NULL,
-  text VARCHAR NOT NULL,
-  price INT NOT NULL,
-  date_of_creation DATE NOT NULL,
-  date_of_complection DATE,
-  type_of_order VARCHAR NOT NULL,
-  status VARCHAR(255) NOT NULL
+  order_id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  customer INT NOT NULL REFERENCES USERS,
+  description TEXT NOT NULL,
+  price DOUBLE PRECISION NOT NULL,
+  date_of_creation TIMESTAMP DEFAULT current_timestamp,
+  date_of_completion TIMESTAMP,
+  order_type VARCHAR(255) NOT NULL,
+  status VARCHAR(255) NOT NULL /* set default*/
 );
+
 CREATE TABLE MESSAGES(
   message_id SERIAL PRIMARY KEY,
-  text_of_message VARCHAR NOT NULL,
-  date_of_sending DATE NOT NULL,
-  sender_of_message INT NOT NULL REFERENCES USERS,
-  recipient_of_message INT NOT NULL REFERENCES USERS,
-  type VARCHAR NOT NULL,
-  status VARCHAR NOT NULL
+  text_of_message TEXT NOT NULL,
+  date_of_sending TIMESTAMP DEFAULT current_timestamp,
+  sender INT NOT NULL REFERENCES USERS,
+  recipient INT NOT NULL REFERENCES USERS,
+  type VARCHAR(255) NOT NULL,
+  status VARCHAR(255) NOT NULL /* set default*/
 );
 
 CREATE TABLE PAYMENTS(
   payment_id SERIAL PRIMARY KEY,
-  ammount INT NOT NULL,
-  date_of_payment DATE NOT NULL,
-  order_id_of_payment INT NOT NULL REFERENCES ORDERS,
-  status VARCHAR(255) NOT NUll
+  ammount DOUBLE PRECISION NOT NULL,
+  date_of_payment TIMESTAMP DEFAULT current_timestamp,
+  order_id INT NOT NULL REFERENCES ORDERS,
+  status VARCHAR(255) NOT NULL /* set default*/
 );
-CREATE TABLE FEEDBACKS(
+
+CREATE TABLE FEEDBACK(
   feedback_id SERIAL PRIMARY KEY,
-  date_of_feedback DATE NOT NULL,
-  text_of_feedback VARCHAR NOT NULL,
-  sender_of_feedback INT NOT NULL REFERENCES USERS,
-  recipient_of_feedback INT NOT NULL REFERENCES USERS,
-  order_id_of_feedback INT NOT NULL REFERENCES ORDERS
+  date_of_feedback TIMESTAMP DEFAULT current_timestamp,
+  text_of_feedback TEXT NOT NULL,
+  sender INT NOT NULL REFERENCES USERS,
+  recipient INT REFERENCES USERS,
+  order_id INT REFERENCES ORDERS
 );
+
 CREATE TABLE ATTACHMENTS(
   attachment_id SERIAL PRIMARY KEY,
-  link VARCHAR,
-  message_id_of_attachment INT NOT NULL REFERENCES MESSAGES,
-  order_id_of_attachment INT NOT NULL REFERENCES ORDERS
+  link VARCHAR(255) NOT NULL,
+  message_id INT REFERENCES MESSAGES,
+  order_id INT REFERENCES ORDERS
 );
+
 create table USER_SKILLS(
-  userskill_user_id INT REFERENCES USERS,
-  userskill_skill_id INT REFERENCES SKILLS
+  user_id INT REFERENCES USERS,
+  skill_id INT REFERENCES SKILLS
 );
-CREATE TABLE USER_ORDERS(
-  user_order_user_id INT REFERENCES USERS,
-  user_order_order_id INT REFERENCES ORDERS
+
+CREATE TABLE TEAMS (
+  team_id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  user_id INT REFERENCES USERS,
+  order_id INT REFERENCES ORDERS
 );

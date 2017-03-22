ec5da4ad11354602b9335eba4475d3ea4393b682
yakovalenko <jaroslawkovalenko@jmail.com>
2017-03-22 12:15:20 +0200

    SKILL pojo is done

--- logic/src/main/java/com/freelancecraft/entities/Skill.java
+++ logic/src/main/java/com/freelancecraft/entities/Skill.java
@@ -1,4 +1,58 @@
 package com.freelancecraft.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "SKILLS")
 public class Skill {
+
+    @Id
+    @Column(name = "skill_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int skillId;
+
+    @Column(name = "name")
+    private String name;
+
+    public int getSkillId() {
+        return skillId;
+    }
+
+    public void setSkillId(int skillId) {
+        this.skillId = skillId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Skill skill = (Skill) o;
+
+        if (skillId != skill.skillId) return false;
+        return name.equals(skill.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = skillId;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "skillId=" + skillId +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- logic/src/main/java/com/freelancecraft/entities/User.java
+++ logic/src/main/java/com/freelancecraft/entities/User.java
@@ -1,5 +1,8 @@
 package com.freelancecraft.entities;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "USERS")
 public class User {
 }

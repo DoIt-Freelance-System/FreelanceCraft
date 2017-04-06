a1b6c40a9ec6b1a6bb6861e3a0d611e8dc3063ee
yakovalenko <jaroslawkovalenko@jmail.com>
2017-04-06 13:20:07 +0300

    AbstractEntity class

--- storage/src/main/java/com/freelancecraft/entities/AbstractEntity.java
+++ storage/src/main/java/com/freelancecraft/entities/AbstractEntity.java
@@ -1,4 +1,18 @@
 package com.freelancecraft.entities;
 
 public class AbstractEntity {
+
+protected int id;
+
+    public AbstractEntity() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
